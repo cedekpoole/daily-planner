f2591a1dea187eda9e0ea7ca382519d0df50a0a8
cedekpoole <cameron.edek.poole@gmail.com>
2024-09-30 12:04:19 +0100

    update logic in stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,12 @@
-export default function Stats() {
+export default function Stats({ stats }) {
+  const itemsDone = stats.filter((item) => item.done).length;
+
   return (
     <footer className="bg-[#76c7ad] text-center font-bold py-8">
-      <em>You have x items in your list, and you already have done X (X%)</em>
+      <em>
+        You have {stats.length} items in your list, and you already have done{" "}
+        {itemsDone} ({(itemsDone / stats.length) * 100}%)
+      </em>
     </footer>
   );
 }
